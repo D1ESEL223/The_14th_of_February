9eec3f601066009c774bbd4a26d7f3cb5456ec50
Danil <dan2004degvl@mail.ru>
2023-02-13 01:50:00 +1000

    1.4 Bug fix

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
+//
+//  main.cpp
+//  Heart for Vlada
+//
+//  Created by Danil D.(20.04.2004) on 12.02.2023.
+//
 #include <iostream>
 #include <cmath>
 #include <string>
-
 using namespace std;
 
 int main() {
-    string s = "";
+    string s = "For Vlada<3";
     for( float y = 1.3 ; y >= -1.1 ; y -= 0.06 ){
         int index = 0;
         for( float x = -1.2 ; x <= 1.2 ; x += 0.025 )
@@ -18,3 +23,4 @@ int main() {
 
     return 0;
 }
+//Finished by Danil D.(20.04.2004) on 13.02.2023
